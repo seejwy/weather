dcfd25fb2ddc264d74842b706018aa9734bdff7c
seejwy <jinyoon@broadwayinc.com>
2024-01-12 13:10:39 +0800

    Reset city on search

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -3,12 +3,13 @@ import './SearchField.css'
 import Search from '../../assets/images/Search.png';
 
 export default function SearchField({onSearch}) {
-  const [city, setCity] = useState('Singapore');
+  const [city, setCity] = useState('');
   const [country, setCountry] = useState('SG');
 
   async function handleSubmit(e) {
     e.preventDefault();
     onSearch(city);
+    setCity('');
   }
 
   function handleCityInputChange(e) {
@@ -19,7 +20,7 @@ export default function SearchField({onSearch}) {
 		<form onSubmit={handleSubmit}>
       <label htmlFor="country-input">
         <div className="label">Country</div>
-        <input id="country-input" type="text" onInput={handleCityInputChange} />
+        <input id="country-input" type="text" value={city} onInput={handleCityInputChange} />
       </label>
 			<button type="submit">
         <span className="material-symbols-outlined">search</span>
